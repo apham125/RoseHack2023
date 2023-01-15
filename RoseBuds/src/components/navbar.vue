4d33cbb4c240e0f25f15718c0e45eaa7549a26e0
Gabriela Alvarez <galva041@ucr.edu>
2023-01-15 00:08:33 -0800

    navbar

---
+++ RoseBuds/src/components/navbar.vue
@@ -0,0 +1,38 @@
+<template>
+    <nav>
+        <!-- <div id="everything"> -->
+            <div id="logo">
+                R
+                <a href="#" ><img src="../assets/roselogo.png"></a> 
+                seBuds
+            </div>
+            
+            <div id="text">
+                <RouterLink to="/">Home</RouterLink>
+                <RouterLink to="/about">About</RouterLink>
+            </div>
+        <!-- </div> -->
+    </nav>
+</template>
+
+<style>
+nav {
+    width: 100%;
+    background-color: #1C0D18;
+    display: flex;
+    justify-content: space-between;
+    
+}
+
+#logo {
+    /* background-color: red; */
+    color: #ffff;
+    font-weight: 200;
+}
+
+#text {
+    /* background-color: blue; */
+    text-align: right;
+}
+
+</style>
