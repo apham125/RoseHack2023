9f999699e42034f2cf423dfce92465f832e28721
Gabriela Alvarez <galva041@ucr.edu>
2023-01-14 21:01:16 -0800

    firebase stuff

---
+++ RoseBuds/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBA9XLlvf0NoKwSCtwfGhA7ZujbFz8J6fs",
+  authDomain: "rosebuds-55844.firebaseapp.com",
+  projectId: "rosebuds-55844",
+  storageBucket: "rosebuds-55844.appspot.com",
+  messagingSenderId: "1034316973615",
+  appId: "1:1034316973615:web:46aed93b0ccf45d9a628da"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
